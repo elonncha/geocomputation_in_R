1768e697a6f8094d5d7ec661db45ed5a852e9451
Yilun 'Elon' Zha <54544200+elonncha@users.noreply.github.com>
2020-05-26 00:32:48 -0400

    add files

---
+++ librarySetUp.R
@@ -0,0 +1,12 @@
+
+librarySetUp = function () {
+  library(leaflet)
+  library(sf)
+  library(tmap)
+  library(spdep)
+  library(raster)
+  library(spData)
+  library(spDataLarge)
+  library(RColorBrewer)
+  library(units)
+}
